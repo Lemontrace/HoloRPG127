fc58c8c407a918a5ad52f8432b1f0aeff7159857
faris.pradiatma <faris.pradiatma@shintavr.com>
2022-04-10 13:18:41 +0700

    Mori Calliope skillset

---
+++ Assets/Scripts/Scripts/ScytheAttack.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScytheAttack : MonoBehaviour
+{
+    float timer = 0f;
+    float dir = 1f;
+    float y = 0f;
+    float z = 0f;
+    float speed = 0f;
+
+    public List<Collider2D> listOfEnemies = new List<Collider2D>();
+    public List<Collider2D> attackedEnemies = new List<Collider2D>();
+
+    private void Start()
+    {
+        dir = CharacterMovement.instance.directionIndex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (timer < 0.3f)
+            timer += Time.deltaTime;
+        else
+            Destroy(gameObject);
+
+            transform.Rotate(new Vector3(0, 0,  -1 * 500 * Time.deltaTime));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            listOfEnemies.Add(collision);
+
+            for (int i = 0; i < listOfEnemies.Count; i++)
+            {
+                if (attackedEnemies.Contains(listOfEnemies[i]))
+                    continue;
+
+                else
+                {
+                    attackedEnemies.Add(collision);
+                    attackedEnemies[i].gameObject.name = "Enemy Hit";
+                    //foreach (var enemy in collision.GetComponents<IEnemy>())
+                    //    enemy.EnemyAttacked();
+
+                }
+            }
+        }
+    }
+}
