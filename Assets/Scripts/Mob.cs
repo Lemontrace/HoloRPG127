e46aa80ed83f5421ff8626614197cafd46f34e2e
Lemontrace <jsh010322@gmail.com>
2022-07-07 08:29:00 +0900

    base classes for mobs

---
+++ Assets/Scripts/Mob.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+abstract public class Mob : MonoBehaviour
+{
+    protected bool Aggroed = false;
+    protected float UnAggroRadious = 7f;
+    protected virtual void Update()
+    {
+        if (Aggroed) onAggroUpdate();
+        else onIdleUpdate();
+    }
+
+    abstract protected void onAggroUpdate();
+    abstract protected void onIdleUpdate();
+}
---
+++ Assets/Scripts/PassiveMob.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class PassiveMob : Mob
+{
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        float distanceToPlayer = Vector2.Distance((Vector2)player.transform.position, (Vector2)transform.position);
+
+        if (distanceToPlayer > UnAggroRadious) Aggroed = false;
+
+        base.Update();
+    }
+
+}
