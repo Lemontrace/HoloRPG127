1ba3f22e32fbf557ab296dd03c6aec0673eed79b
Alpha <bjkim9805@gmail.com>
2022-08-04 20:18:54 +0900

    OptionStatus Upload

---
+++ Assets/Scripts/System/OptionStatus.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionStatus
+{
+    private static double sound;
+    private static double brightness;
+}
